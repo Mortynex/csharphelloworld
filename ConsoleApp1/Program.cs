12e11c2473e3a8d6683f27542abb84dc72c1f3d8
Polťák Martin <poltak.ma.2022@skola.ssps.cz>
2022-09-21 11:11:38 +0200

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.Write("napis neco kokote: ");
+
+            String penis = Console.ReadLine();
+
+            if (penis.ToLower().Contains("martin"))
+            {
+                Console.Write("naser si");
+                Console.ReadKey(true);
+                return;
+
+            }
+
+            while (true) {
+                Console.WriteLine(penis);
+                System.Threading.Thread.Sleep(50);
+            }*/
+
+            while (true)
+            {
+                Console.Write("pravda nebo lez: ");
+                String penis = Console.ReadLine();
+
+
+                if (penis.ToLower().Contains("martin"))
+                {
+                    Console.Write("lež");
+                    Console.ReadKey(true);
+                    return;
+
+                }
+                else
+                {
+                    Console.Write("pravda");
+                    Console.ReadKey(true);
+                    return;
+                }
+            }
+        }
+    }
+}
